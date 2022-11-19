07dbe4f836190dec785da1aeeccfebe715223934
Dewa Semadi <dewasemadi3@gmail.com>
2022-11-19 08:13:56 +0700

    fix error build

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -28,12 +28,7 @@ export default function AddTodo(props: addTodoProps) {
             </div>
             {/*body*/}
             <div className='relative p-6 flex-auto'>
-              <p className='my-4 text-slate-500 text-lg leading-relaxed'>
-                I always felt like I could do anything. That’s the main thing people are controlled
-                by! Thoughts- their perception of themselves! They're slowed down by their
-                perception of themselves. If you're taught you can’t do anything, you won’t do
-                anything. I was taught I could do everything.
-              </p>
+              <p className='my-4 text-slate-500 text-lg leading-relaxed'></p>
             </div>
             {/*footer*/}
             <div className='flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b'>
